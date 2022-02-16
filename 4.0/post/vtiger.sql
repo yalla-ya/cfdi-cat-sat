698677603102e3d1ea24a252ad07955c54bdb3b8
Ido Kobelkowsky <ido@yalla-ya.com>
2022-02-16 20:46:48 +0200

    vtiger.aql

--- 4.0/post/vtiger.sql
+++ 4.0/post/vtiger.sql
@@ -25,13 +25,21 @@ FROM `sat_cat_c_metodo_pago` );
 RENAME TABLE `vtiger_cfdi_use` TO `_z_archive_vtiger_cfdi_use`;
 RENAME TABLE `vtiger_cfdi_use_seq` TO `_z_archive_vtiger_cfdi_use_seq`;
 
-CREATE VIEW `vtiger_cfdi_use` AS ( SELECT 
+CREATE VIEW `vtiger_cfdi_use` AS SELECT 
+	0 AS `cfdi_useid`, 
+    '' AS `cfdi_use`,
+	0 AS `sortorderid`,
+	1 AS `presence`,
+    NULL AS `color`,
+    0 AS `picklist_valueid`
+UNION SELECT 
 	ROW_NUMBER() AS `cfdi_useid`,
 	`description` AS `cfdi_use`,
 	0 AS `sortorderid`,
 	1 AS `presence`,
-	NULL AS `color`
-FROM `sat_cat_c_uso_cfdi` ); 
+	NULL AS `color`,
+	0 AS `picklist_valueid`
+FROM `sat_cat_c_uso_cfdi`; 
 
 RENAME TABLE `vtiger_fisical_status` TO `_z_archive_vtiger_fisical_status`;
 RENAME TABLE `vtiger_fisical_status_seq` TO `_z_archive_vtiger_fisical_status_seq`;
@@ -60,13 +68,22 @@ FROM `sat_cat_c_tipo_de_comprobante` );
 RENAME TABLE `vtiger_cfdi_relation` TO `_z_archive_vtiger_cfdi_relation`;
 RENAME TABLE `vtiger_cfdi_relation_seq` TO `_z_archive_vtiger_cfdi_relation_seq`;
 
-CREATE VIEW `vtiger_cfdi_relation` AS ( SELECT 
+CREATE VIEW `vtiger_cfdi_relation` AS SELECT 
+	0 AS `cfdi_relationid`, 
+    '' AS `cfdi_relation`,
+	0 AS `sortorderid`,
+	1 AS `presence`,
+    NULL AS `color`,
+    0 AS `picklist_valueid`
+UNION
+SELECT 
 	CAST( `document_relation` AS UNSIGNED ) AS `cfdi_relationid`,
 	`description` AS `cfdi_relation`,
 	CAST( `document_relation` AS UNSIGNED ) AS `sortorderid`,
 	1 AS `presence`,
-	NULL AS `color`
-FROM `sat_cat_c_tipo_relacion` ); 
+	NULL AS `color`,
+    0 AS `picklist_valueid`
+FROM `sat_cat_c_tipo_relacion`; 
 
 
 RENAME TABLE `vtiger_pediment_code` TO `_z_archive_vtiger_pediment_code`;
@@ -114,3 +131,17 @@ UPDATE vtiger_units, vtiger_unitscf, sat_cat_c_clave_unidad
         vtiger_unitscf.end_date = STR_TO_DATE( sat_cat_c_clave_unidad.end_date, '%d/%m/%y')
 WHERE vtiger_units.unitid = vtiger_unitscf.unitid
 	AND vtiger_units.code = sat_cat_c_clave_unidad.unit_code; 
+
+-- UPDATE role permission to allow 0 for all existing roles
+UPDATE IGNORE `vtiger_role2picklist` SET `picklistvalueid` = 0 WHERE `picklistid` IN
+(
+	SELECT *
+    FROM `vtiger_picklist` 
+    WHERE `name` 
+		IN ( 'cfdi_relation',  'cfdi_use', 'fisical_status', 'payment_form', 'payment_method', 'pediment_code', 'type' )
+);
+
+REPLACE INTO `vtiger_role2picklist` ( SELECT roleid, 0, picklistid, 0 FROM `vtiger_role`, `vtiger_picklist` 
+WHERE `vtiger_picklist`.`name` 
+		IN ( 'cfdi_relation',  'cfdi_use', 'fisical_status', 'payment_form', 'payment_method', 'pediment_code', 'type' )
+);

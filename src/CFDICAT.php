95963edea7827ad20eab20a95212209a5c5ab073
ido Kobelkowsky <ido@yalla-ya.com>
2022-03-30 16:10:07 +0300

    Update CFDICAT.php

--- src/CFDICAT.php
+++ src/CFDICAT.php
@@ -16,10 +16,23 @@ namespace CFDICAT;
 
 class CFDICAT {
 
-    public static function check( $value, $catalog, $return = 'code' ) {
-        define( 'CFDICAT_ROOT', dirname( dirname( __FILE__ ) ) );
-
-        print $CFDICAT_ROOT;
+    public static function check( $value, $catalog, $return = true, $version = null ) {
+        if ( $version == null ) $version = $_ENV[ 'CFDI_VERSION' ];
+        $file = dirname( __FILE__ ) . '/../' . $version . '/json/' . self::decamelize( $catalog ) .'.t.json';
+        $catalog = json_decode( file_get_contents( $file ), true );
+        if ( $return === true ) {
+            $return = array_keys( $catalog[ 0 ] )[ 0 ];
+        }
+        $keys = array_column( $catalog, $return );
+        if ( !count( $keys ) ) return( null );
+        $key = array_search( $value, $keys );
+        if ( $key === false ) return( null );
+        return( $catalog[ $key ][ $return ] );
     }
 
+
+    public static function decamelize( $string ) {
+        return( str_replace( '__', '_', ltrim( strtolower( preg_replace( '/[A-Z]([A-Z](?![a-z]))*/', '_$0', $string ) ), '_' ) ) );
+    }
+    
 }
